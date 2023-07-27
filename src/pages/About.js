aa3d94b3ac285543fe707c59b8d7ce27e0837ebc
lallemand-fred <114465446+lallemand-fred@users.noreply.github.com>
2023-07-27 08:59:13 +0200

    Développement de la navigation

---
+++ src/pages/About.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>A propos</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati
+        vitae voluptatem ipsum minus alias cupiditate commodi nesciunt, ullam
+        fugiat accusantium, magni sunt ducimus tempore quaerat nam soluta
+        corrupti reprehenderit deleniti dolore. Nobis incidunt eum, dolor fugiat
+        illo ipsa illum eveniet autem sed expedita dolorum cupiditate
+        voluptatem, vero eaque exercitationem mollitia, eius libero? Tempore
+        quaerat similique voluptates? Repellat quidem quas itaque, quam
+        mollitia, neque cumque perspiciatis corporis quaerat labore blanditiis
+        rem!
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas enim
+        laborum quaerat eligendi pariatur delectus incidunt esse, vel
+        praesentium corrupti nihil, assumenda cupiditate quia aspernatur ipsa
+        quasi impedit beatae! Officia, expedita repellendus distinctio dicta
+        ipsam quod magnam rerum autem corrupti eaque iste mollitia sequi non,
+        cupiditate esse perferendis ex ratione praesentium. Enim, sapiente
+        ratione. Eligendi fuga eos minima earum ducimus ab, quo molestiae velit
+        eius quaerat, deserunt excepturi ut! Odit dolor, exercitationem
+        aspernatur nostrum laborum voluptate quia et ducimus voluptatum, magni
+        minima obcaecati quos accusamus tempore excepturi fugit saepe. Dolorem
+        ex assumenda suscipit ipsum voluptatum, reprehenderit molestias nemo
+        aliquid facere.
+      </p>
+    </div>
+  );
+};
+
+export default About;
