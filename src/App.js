63075b7aa7932d374ff2f1fb0918cb08ff00b33e
lallemand-fred <114465446+lallemand-fred@users.noreply.github.com>
2023-07-27 08:59:32 +0200

    Modiff App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import About from "./pages/About";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        {/* le path="*" si l'url ne correspond à rien de déclaré au dessus, te re dirige vers la page home*/}
+        <Route path="/*" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
